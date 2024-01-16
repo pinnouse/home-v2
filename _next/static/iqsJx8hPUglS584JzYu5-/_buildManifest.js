043a5c4cf96caaba9bb924acb0850ebd5efdff91
pinnouse <cupofpho@gmail.com>
2024-01-16 18:15:02 +0000

    Deploying to gh-pages from @ pinnouse/home-v2@9c1e2ffb4716674a8a3b63478f753abd891285ff 🚀

--- _next/static/iqsJx8hPUglS584JzYu5-/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/495-de74ffd67b6f09fd.js","static/css/a9d56130b7e547f9.css","static/chunks/pages/index-5d7afe4329459127.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
---
+++ _next/static/ovGsQHcMXCSU9EnrYgUJY/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/495-de74ffd67b6f09fd.js","static/css/a9d56130b7e547f9.css","static/chunks/pages/index-7eac10fe7379a641.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();

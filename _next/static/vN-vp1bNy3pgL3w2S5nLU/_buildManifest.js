93023e5f0ac406414fa089d868aefa94116f693d
pinnouse <cupofpho@gmail.com>
2024-08-21 22:25:54 +0000

    Deploying to gh-pages from @ pinnouse/home-v2@869b50eb19e591fb2b9c350a8b6ce3fdfd53f6d8 🚀

---
+++ _next/static/vN-vp1bNy3pgL3w2S5nLU/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/121-dc9bc5c0e3ec5b01.js","static/css/01eb6ef8132e3bdd.css","static/chunks/pages/index-abd5c7a792f40464.js"],"/_error":["static/chunks/pages/_error-77823ddac6993d35.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
